3329bfa17fa26d5252ce7a23182ad194591eb807
Harshvardhansisodia <sisodia.0304@gmail.com>
2022-07-15 17:14:51 +0530

    updated

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT =process.env.PORT || 3000;
+require('dotenv').config();
+
+mongoose.connect(process.env.url,{useNewUrlParser: true,
+useUnifiedTopology: true}).then(
+    () => {
+        console.log('Connected to MongoDB');
+    }  
+).catch(
+    (err) => {
+        console.log('Error connecting to MongoDB: ', err.message);
+    }
+);
+const db = mongoose.connection.useDb('sample_guides').collection('planets');
+
+app.get('/data', (req, res) => {
+
+    db.find().toArray(
+      (err, result)=>{
+        if(err){
+          console.log(err);
+        }
+        res.status(200).json(result);
+        console.log(result);
+      }
+    )
+});
+
+
+app.listen(
+  PORT,
+  () => { console.log(`Server is live at ${PORT}`); }
+);
